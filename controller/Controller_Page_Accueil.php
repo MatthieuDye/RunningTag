f45c2844c823aa630358d5cbbc5d51d4ac18bf9d
MatthieuDye <matthieudye@yahoo.fr>
2017-05-23 22:32:22 +0200

    Update Controller_Page_Accueil.php

--- controller/Controller_Page_Accueil.php
+++ controller/Controller_Page_Accueil.php
@@ -3,6 +3,6 @@ require_once '/model/User.php';
 
 require_once '/controller/Controller_Test_User.php';
 
-echo file_get_contents( '/view/accueil.php') ;
+require( '/view/accueil.php') ;
 //onlineOnly();
-?>
+
